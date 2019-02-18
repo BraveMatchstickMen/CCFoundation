2833437c1adf0376e2bb0ef4642ee51dfc16fdc0
chai <chai.chai@farfetch.com>
2019-02-18 18:50:30 +0800

    init project

---
+++ CCFoundation/CCFoundation.h
@@ -0,0 +1,19 @@
+//
+//  CCFoundation.h
+//  CCFoundation
+//
+//  Created by chai.chai on 2019/2/18.
+//  Copyright © 2019 chai.chai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CCFoundation.
+FOUNDATION_EXPORT double CCFoundationVersionNumber;
+
+//! Project version string for CCFoundation.
+FOUNDATION_EXPORT const unsigned char CCFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CCFoundation/PublicHeader.h>
+
+
---
+++ CCFoundationDemo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CCFoundationDemo
+//
+//  Created by chai.chai on 2019/2/18.
+//  Copyright © 2019 chai.chai. All rights reserved.
+//
+
+import UIKit
+import CCFoundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
